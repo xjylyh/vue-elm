7d06b6541aaa1d7110a4524052119037b70b1fc0
xiejiyao <470267864@qq.com>
2017-11-12 21:55:11 +0800

    component header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -26,6 +26,9 @@
 
     </div>
     <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
     </div>
   </div>
 </template>
